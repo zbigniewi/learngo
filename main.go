1a99712648314d37cd83b8c897468fc4c97058da
Zbigniew Itrich <zbigniew.itrich@schanksysteme.com>
2023-10-12 16:24:39 +0200

    Rendering the FAQ page with a gohtml template.

--- main.go
+++ main.go
@@ -37,24 +37,8 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func faqtHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>Faq Page </h1><p>
-	<ul>
-		<li>
-			<b>Is there a free version?</b>
-			Yes! We offer a free trial for 30 days on any paid plans.
-		</li>
-		<li>
-			<b>What are your support hours?</b>
-			We have support staff answering emails 24/7, though response
-			times may be a bit slower on weekends.
-		</li>
-		<li>
-			<b>How do I contact support?</b>
-			Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
-		</li>
-	</ul>
-	`)
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
+
 }
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
