0b1dcb80fdd43ea4e024b90fad0850c03b55fdf4
Zbigniew Itrich <zbigniew.itrich@schanksysteme.com>
2023-10-12 09:07:21 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my first site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page </h1><p>To get in touch, email me at <a href=\"mailto:zbigniew.itrich@schanksysteme.com\">zbigniew.itrich@schanksysteme.com</a>.")
+}
+
+func faqtHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>Faq Page </h1><p>
+	<ul>
+		<li>
+			<b>Is there a free version?</b>
+			Yes! We offer a free trial for 30 days on any paid plans.
+		</li>
+		<li>
+			<b>What are your support hours?</b>
+			We have support staff answering emails 24/7, though response
+			times may be a bit slower on weekends.
+		</li>
+		<li>
+			<b>How do I contact support?</b>
+			Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+		</li>
+	</ul>
+	`)
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// handle the page not found error
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
+type Router struct{}
+
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	case "/faq":
+// 		faqtHandler(w, r)
+// 	default:
+// 		// handle the page not found error
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// }
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqtHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+	fmt.Println("Starting the server on :3000")
+	http.ListenAndServe(":3000", r)
+}
