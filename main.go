1d1257fb36a4be7c1b2cef2ad2c32a11de4b78c3
Zbigniew Itrich <zbigniew.itrich@schanksysteme.com>
2023-10-20 14:41:49 +0200

    Created the users controller and moved the signup page over to it.

--- main.go
+++ main.go
@@ -21,25 +21,32 @@ func (st SomeType) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS,
-			"home.gohtml", "tailwind.gohtml"))))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"home.gohtml", "tailwind.gohtml",
+	))))
 
-	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS,
-			"contact.gohtml", "tailwind.gohtml"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"contact.gohtml", "tailwind.gohtml",
+	))))
 
-	r.Get("/faq", controllers.FAQ(
-		views.Must(views.ParseFS(templates.FS,
-			"faq.gohtml", "tailwind.gohtml"))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(
+		templates.FS,
+		"faq.gohtml", "tailwind.gohtml",
+	))))
 
-	r.Get("/signup", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS,
-			"signup.gohtml", "tailwind.gohtml"))))
+	usersC := controllers.Users{}
+	usersC.Templates.New = views.Must(views.ParseFS(
+		templates.FS,
+		"signup.gohtml", "tailwind.gohtml",
+	))
+	r.Get("/signup", usersC.New)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
+
 	fmt.Println("Starting the server on :3000")
 	http.ListenAndServe(":3000", r)
 }
